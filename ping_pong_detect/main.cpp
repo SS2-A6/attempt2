6f291b70e10c6e67fc4c7706b70ab5e92f073488
Yamashita <under_mt_d@yahoo.co.jp>
2016-12-05 16:17:34 +0900

    ping pong psd

--- ping_pong_detect/main.cpp
+++ ping_pong_detect/main.cpp
@@ -1,13 +1,59 @@
 #include "mbed.h"
+//#include "rtos.h"
 
-DigitalOut led1(LED1);
+/*
+Semaphore slots(1);
+int test = 0;
 
-// main() runs in its own thread in the OS
-// (note the calls to Thread::wait below for delays)
+void some_work(){
+
+	for (int i=0;i<100000;i++) { slots.wait(); test++; slots.release(); }
+
+}
+
+void some_work2(){
+
+	for (int i=0;i<100000;i++) { test++; }
+
+}
+*/
+
+AnalogIn myPSD(PA_0);
+Serial pc(USBTX, USBRX);
+PwmOut myLED(LED1);
+#define offset 8000
 int main() {
+
+	uint16_t ball_psd;
+	uint16_t ball_psd_before = 0;
+	int16_t ball_diff;
+	uint16_t ball_count = 0;
+	uint16_t ball_flag = 0;
+
     while (true) {
-        led1 = !led1;
-        Thread::wait(500);
+
+        ball_psd = myPSD.read_u16();
+        ball_diff = ball_psd - ball_psd_before;
+        ball_psd_before = ball_psd;
+        pc.printf("%d %d\n", ball_psd, ball_count);
+        //myLED = val/65535.0f;
+        //Thread::wait(30);
+
+        if ( ball_flag == 0 ) {
+        	if ( ball_diff < -offset ) {
+        		ball_flag = 1;
+        	}
+        }
+        else {
+        	if ( ball_diff > offset ) {
+        		ball_flag = 0;
+        		ball_count ++;
+        		Thread::wait(10);
+        		//pc.printf("%d\n", ball_count);
+        	}
+        }
+
     }
+
 }
 
