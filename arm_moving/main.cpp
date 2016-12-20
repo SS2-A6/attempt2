c5195a40f9aece8cfdbb709deca40b84fc36aaa1
Yamashita <under_mt_d@yahoo.co.jp>
2016-12-20 10:50:25 +0900

    motor moving

--- arm_moving/main.cpp
+++ arm_moving/main.cpp
@@ -2,9 +2,15 @@
 #include <math.h>
 
 Serial pc(USBTX, USBRX);
+Serial xbee();
 PwmOut servo(PB_3);
+DigitalIn mag(PB_5);
+PwmOut motor1(PA_5);
+PwmOut motor2(PA_6);
+
 float make_arctan ( int8_t from_degree, int8_t to_degree, uint16_t t, uint16_t max_time, float x_p, float y_p );
 void moving_arm ( int8_t from_degree, int8_t to_degree, uint16_t max_time, float x_p, float y_p );
+void moving ( uint8_t move_mode, uint8_t time_mode, uint16_t move_time );
 
 
 int main() {
@@ -12,20 +18,81 @@ int main() {
 	pc.baud(115200);  // ボーレートの設定
 	servo.period(0.014);  // サーボの周期
 
-	// サーボ実験データ (Futaba S3003, 5V)
-	//-90 : servo.pulsewidth(0.00060);
-	// 00 : servo.pulsewidth(0.00140);
-	//+90 : servo.pulsewidth(0.00220);
+	if (  ) {
 
-	// テスト
-	moving_arm( -60, 60, 3000, 25, 90);
-	moving_arm( 60, -60, 3000, 25, 90);
+	}
+
+}
+
+
+// テスト用関数
+void debug_test () {
+
+	// アーム移動テスト
+	//moving_arm( 0, -45, 3000, 30, 90);
+	//moving_arm( -45, 0, 2000, 40, 90);
+
+	/*
+	// マグネットテスト
+	while ( true ) {
+		uint8_t mag_val = mag.read();
+		//pc.printf("%d\n", mag_val);
+
+		if ( mag_val == 0 ) {
+			moving(1,0,0);
+		}
+		else if ( mag_val == 1 ) {
+			moving(0,0,0);
+			break;
+		}
+	}
+	*/
+
+	// レール移動テスト
+	//moving(1, 1, 3000);
+	//moving(2, 1, 3000);
+
+}
+
+
+// レール移動関数
+// 引数 ( 移動モード, 時間モード, 移動時間 )
+// 移動モード 0：停止，1：前進，2：後進
+// 時間モード 0：時間指定しない，1：時間指定する
+// 移動時間 (ms)
+void moving ( uint8_t move_mode, uint8_t time_mode, uint16_t move_time ) {
+
+	if ( move_mode == 0 ) {
+		motor1.pulsewidth(0.0);
+		motor2.pulsewidth(0.0);
+		if ( time_mode == 1 ) {
+			Thread::wait(move_time);
+		}
+	}
+	else if ( move_mode == 1 ) {
+		motor1.pulsewidth(0.0);
+		motor2.pulsewidth(1.0);
+		if ( time_mode == 1 ) {
+			Thread::wait(move_time);
+			motor1.pulsewidth(0.0);
+			motor2.pulsewidth(0.0);
+		}
+	}
+	else if ( move_mode == 2 ) {
+		motor1.pulsewidth(1.0);
+		motor2.pulsewidth(0.0);
+		if ( time_mode == 1 ) {
+			Thread::wait(move_time);
+			motor1.pulsewidth(0.0);
+			motor2.pulsewidth(0.0);
+		}
+	}
 
 }
 
 
 // アーム移動関数
-// 引数 ( スタート角度, ゴール角度, 移動目標時間, 中間滑らか度, 漸近度 )
+// 引数 ( スタート角度, ゴール角度, 移動完了時間, 中間滑らか度, 漸近度 )
 // 中間滑らか度 (中間傾斜区間を全体の何%ほどにするか)
 // 漸近度 (漸近区間を真値に何%ほど近づけるか．サーボ安全率も担保するため固定推奨)
 void moving_arm ( int8_t from_degree, int8_t to_degree, uint16_t max_time, float x_p, float y_p ) {
@@ -41,15 +108,23 @@ void moving_arm ( int8_t from_degree, int8_t to_degree, uint16_t max_time, float
 
 
 // アークタンジェント自動生成関数
-// 引数 ( スタート角度, ゴール角度, 移動現在時刻, 移動目標時間, 中間滑らか度, 漸近度 )
+// 引数 ( スタート角度, ゴール角度, 移動現在時刻, 移動完了時間, 中間滑らか度, 漸近度 )
 // 中間滑らか度 (中間傾斜区間を全体の何%ほどにするか)
 // 漸近度 (漸近区間を真値に何%ほど近づけるか．サーボ安全率の担保のため固定推奨!)
 float make_arctan ( int8_t from_degree, int8_t to_degree, uint16_t t, uint16_t max_time, float x_p, float y_p  ) {
 
-	float c = 100/(max_time*x_p) * tan( M_PI*y_p/100 - M_PI/2  );  // x_p, y_pから横軸圧縮率cを自動計算
+	float c = 100/(max_time*x_p) * tan( M_PI*y_p/100 - M_PI/2  );  // x_p, y_pからグラフ圧縮率cを計算
 	float from_val = 1.0e-5 * from_degree + 1.4e-3;  // スタート角度からサーボ用数値へ変換
 	float to_val = 1.0e-5 * to_degree + 1.4e-3;  // ゴール角度からサーボ用数値へ変換
 
 	return (to_val - from_val)/M_PI * atan( c*(t - max_time/2.0) ) + (from_val + to_val)/2.0;
 
 }
+
+
+///////////////////////////////////
+// サーボ実験データ (Futaba S3003, 5V)
+//-90 : servo.pulsewidth(0.00060);
+// 00 : servo.pulsewidth(0.00140);
+//+90 : servo.pulsewidth(0.00220);
+///////////////////////////////////
